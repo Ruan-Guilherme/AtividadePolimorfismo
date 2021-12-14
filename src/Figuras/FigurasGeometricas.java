1d3a9209b6b3e0f271b75e15a63588c16f2cb38d
Ruan-Guilherme <ruan.q2004@aluno.ifsc.edu.br>
2021-12-14 16:16:47 -0300

    AtividadePolimorfismo

---
+++ src/Figuras/FigurasGeometricas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Figuras;
+
+/**
+ *
+ * @author IFSC
+ */
+public class FigurasGeometricas {
+  
+    public void Desenha(){
+    System.out.println("Desenhando...");
+    }
+}
---
+++ src/Figuras/Quadrado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Figuras;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Quadrado extends FigurasGeometricas{
+    
+     public void Desenha(){
+    System.out.println("Desenhando Quadrado...");
+    }
+    
+}
